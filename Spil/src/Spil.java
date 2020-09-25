7b8cf3194054ce07c3a9edd382fb44be349c1111
s205341 <s205341@student-dtu.dk>
2020-09-25 13:25:18 +0200

    Intast spillernavne

---
+++ Spil/src/Spil.java
@@ -0,0 +1,32 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class Spil {
+    public static void main(String[] args) {
+        System.out.println("Spiller 1 skriv dit navn:");
+        Scanner sc = new Scanner(System.in);
+
+        String Spiller1 = sc.next();
+        System.out.println("Velkommen " + Spiller1);
+
+
+        System.out.println("Spiller 2 skriv dit navn");
+        String Spiller2 = sc.next();
+        System.out.println("Velkommen " + Spiller2);
+
+
+
+//        System.out.println("Skriv Start for at starte spillet!");
+//        String start
+//        start = start.toLowerCase();
+
+
+    }
+
+
+
+}
+
+
+
+
---
+++ Test/src/Test.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class Test {
+
+
+
+
+
+
+//
+//
+//
+//    private final int MAX = 6; //angiver max antal øjne for terning
+//    private int faceValue; //nuværende øjne
+//
+//    //konstruktør
+//    public Test(int value)
+//    {
+//        faceValue = value;
+//    }
+//
+//   //kaste terning og retuner øjne værdi
+//   public int roll()
+//   {
+//       faceValue = (int)(Math.random() * MAX) +1;
+//       return faceValue;
+//   }
+//
+//   //sæt øjne værdi
+//    public int getFaceValue()
+//    {
+//        return faceValue;
+//    }
+//
+//
+//    //returnerer en streng repræsentationer af objeketes data
+//    public String toString()
+//    {
+//        String result = "øjne : " + faceValue;
+//        return result;
+//    }
+//
+//}
+
+//        public void play1(){
+//
+//            System.out.println("Skriv dit navn.");
+//            Scanner scan = new Scanner(System.in);
+//            String spiller1 = scan.next();
+//            scan.close();
+//            System.out.println("Velkommen " + spiller1);
+//        }
+//
+//    }
+
+
+
+
+
+//public class DiceGame {
+//
+//    private final int MAX = 6;  // maximum øjne værdi
+//
+//    private int faceValue;  // nuværende øjne værdi
+//
+//    // konstruktør
+//    public DiceGame(int value)
+//    {
+//        faceValue = value;
+//    }
+//
+//    // kast terning og returner øjne værdi
+//    public int roll()
+//    {
+//        faceValue = (int)(Math.random() * MAX) + 1;
+//        return faceValue;
+//    }
+//
+//    // sæt øjne værdi
+//    public void setFaceValue (int value)
+//    {
+//        faceValue = value;
+//    }
+//
+//    // hent øjne værdi
+//    public int getFaceValue()
+//    {
+//        return faceValue;
+//    }
+//
+//    // returnerer en streng repræsentation af objektets data
+//    public String toString()
+//    {
+//        String result = "Øjne: " + faceValue;
+//        return result;
+//    }
+//
+//}
+
+
+
+
+}
