8dcaf09e9c3c829b3aa5afb2dfc6b8e1ac212175
s205341 <s205341@student-dtu.dk>
2020-09-25 14:17:45 +0200

    start slut

--- Spil/src/Spil.java
+++ Spil/src/Spil.java
@@ -6,22 +6,8 @@ public class Spil {
        //navn på spiller 1
 
         String another = "y";
-//
-//        System.out.println("Spiller 1 skriv dit navn:");
-//        Scanner sc = new Scanner(System.in);
-//
-//        String Spiller1 = sc.next();
-//        System.out.println("Velkommen " + Spiller1);
-//
-//        //navn på spiller 2
-//        System.out.println("Spiller 2 skriv dit navn");
-//        String Spiller2 = sc.next();
-//        System.out.println("Velkommen " + Spiller2);
 
 
-//        System.out.println("Skriv 'Start' for at starte spillet!");
-//        String start = sc.next();
-//        start = start.toLowerCase();
 
 
       //slut spil
@@ -39,7 +25,19 @@ public class Spil {
            String Spiller2 = sc.next();
            System.out.println("Velkommen " + Spiller2);
 
+           //start spil
+           System.out.println("Skriv 'Start' for at starte spillet!");
+           String start = sc.next();
+           start = start.toLowerCase();
 
+
+
+
+
+
+
+
+           //slut på spil
            System.out.println();
            System.out.print("Vil du spille igen (y/n) ");
            another = sc.next();
