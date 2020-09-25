7222cc7d306e001c9e73da155efc94fcd8f18396
s205341 <s205341@student-dtu.dk>
2020-09-25 14:02:13 +0200

    slut på spil

--- Spil/src/Spil.java
+++ Spil/src/Spil.java
@@ -3,28 +3,48 @@ import java.util.Scanner;
 
 public class Spil {
     public static void main(String[] args) {
-        System.out.println("Spiller 1 skriv dit navn:");
-        Scanner sc = new Scanner(System.in);
+       //navn på spiller 1
 
-        String Spiller1 = sc.next();
-        System.out.println("Velkommen " + Spiller1);
+        String another = "y";
+//
+//        System.out.println("Spiller 1 skriv dit navn:");
+//        Scanner sc = new Scanner(System.in);
+//
+//        String Spiller1 = sc.next();
+//        System.out.println("Velkommen " + Spiller1);
+//
+//        //navn på spiller 2
+//        System.out.println("Spiller 2 skriv dit navn");
+//        String Spiller2 = sc.next();
+//        System.out.println("Velkommen " + Spiller2);
 
 
-        System.out.println("Spiller 2 skriv dit navn");
-        String Spiller2 = sc.next();
-        System.out.println("Velkommen " + Spiller2);
-
-
-
-//        System.out.println("Skriv Start for at starte spillet!");
-//        String start
+//        System.out.println("Skriv 'Start' for at starte spillet!");
+//        String start = sc.next();
 //        start = start.toLowerCase();
 
 
+      //slut spil
+           while (another.equalsIgnoreCase("y"))
+       {
+
+           System.out.println("Spiller 1 skriv dit navn:");
+           Scanner sc = new Scanner(System.in);
+
+           String Spiller1 = sc.next();
+           System.out.println("Velkommen " + Spiller1);
+
+           //navn på spiller 2
+           System.out.println("Spiller 2 skriv dit navn");
+           String Spiller2 = sc.next();
+           System.out.println("Velkommen " + Spiller2);
+
+
+           System.out.println();
+           System.out.print("Vil du spille igen (y/n) ");
+           another = sc.next();
+       }
     }
-
-
-
 }
 
 
